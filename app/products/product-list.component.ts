2378ea282b29026b22ed93f3119fc1333f6aed02
Tane Yoroshi <taneyoroshi@gmail.com>
2016-10-08 23:26:47 +0800

    Retrieved data using http.

--- app/products/product-list.component.ts
+++ app/products/product-list.component.ts
@@ -19,6 +19,7 @@ export class ProductListComponent implements OnInit {
     imageMargin: number = 2;
     showImage: boolean = false;
     listFilter: string;
+    errorMessage: string;
     products: IProduct[];
 
     constructor(private _productService: ProductService){
@@ -30,7 +31,10 @@ export class ProductListComponent implements OnInit {
     }
 
     ngOnInit() : void {
-        this.products = this._productService.getProducts();
+        this._productService.getProducts()
+            .subscribe(
+                products => this.products = products,
+                error => this.errorMessage = <any>error);
     }
 
     onRatingClicked(message: string) : void {
