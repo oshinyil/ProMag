3935afa87666e0b0cd3dacd85e3b77b164a0c0f8
Tane Yoroshi <taneyoroshi@gmail.com>
2016-10-09 11:18:22 +0800

    Displayed product detail by passing parameter to a route.

--- app/app.component.ts
+++ app/app.component.ts
@@ -6,6 +6,7 @@ import { ROUTER_PROVIDERS, RouteConfig, ROUTER_DIRECTIVES } from 'angular2/route
 import { ProductListComponent } from './products/product-list.component';
 import { ProductService } from './products/product.service';
 import { WelcomeComponent } from './home/welcome.component';
+import { ProductDetailComponent } from './products/product-detail.component';
 
 @Component({
     selector: 'pm-app',
@@ -32,7 +33,8 @@ import { WelcomeComponent } from './home/welcome.component';
 })
 @RouteConfig([
     { path: '/welcome', name: 'Welcome', component: WelcomeComponent, useAsDefault: true },
-    { path: '/products', name: 'Products', component: ProductListComponent }
+    { path: '/products', name: 'Products', component: ProductListComponent },
+    { path: '/products/:id', name: 'ProductDetail', component: ProductDetailComponent }
 ])
 export class AppComponent {
     pageTitle: string = "Promag Product Management";
--- app/products/product-detail.component.ts
+++ app/products/product-detail.component.ts
@@ -1,4 +1,5 @@
 import { Component } from 'angular2/core';
+import { RouteParams } from 'angular2/router'
 
 @Component({
     templateUrl: 'app/products/product-detail.component.html'
@@ -6,4 +7,9 @@ import { Component } from 'angular2/core';
 
 export class ProductDetailComponent {
     pageTitle: string = 'Product Detail';
+
+    constructor(private _routeParams: RouteParams) {
+        let id = +this._routeParams.get('id');
+        this.pageTitle += `: ${id}`;
+    }
 }
